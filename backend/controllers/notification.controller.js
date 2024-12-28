bcded217c8469d2fcfc692d23b16bcc8a5dcd140
Amarrv <amarnathrv1927@gmail.com>
2024-12-28 23:34:30 +0530

    3-Notification query and mutation added

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -10,9 +10,10 @@ export const getNotifications = async (req, res) => {
     });
 
     await Notification.updateMany({ to: userId }, { read: true });
+
     res.status(200).json(notifications);
   } catch (error) {
-    console.log("Error in getNotification function", error.message);
+    console.log("Error in getNotifications function", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -20,11 +21,12 @@ export const getNotifications = async (req, res) => {
 export const deleteNotifications = async (req, res) => {
   try {
     const userId = req.user._id;
+
     await Notification.deleteMany({ to: userId });
 
     res.status(200).json({ message: "Notifications deleted successfully" });
   } catch (error) {
-    console.log("Error in deleteNotification function", error.message);
+    console.log("Error in deleteNotifications function", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
